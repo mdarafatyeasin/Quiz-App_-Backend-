ee4ae4e1d5e365514387a66f52bc84c8115df082
mdarafatyeasin <yeasinarafat54239@gmail.com>
2024-04-17 21:28:50 +0600

    update the req

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +27,12 @@ SECRET_KEY = 'django-insecure-tp2@)g1+u%ddya45%0u8(f0&yvhzaxb0iv4hzmptz@ozyrpfpx
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = ['https://*.127.0.0.1']
+
+
+env = environ.Env()
+environ.Env.read_env()
 
 
 # Application definition
@@ -142,5 +149,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER ="yeasinwebcode@gmail.com"
-EMAIL_HOST_PASSWORD ="bvpm ojwx svaj wped"
+EMAIL_HOST_USER =env("EMAIL")
+EMAIL_HOST_PASSWORD =env("EMAIL_PASSWORD")
